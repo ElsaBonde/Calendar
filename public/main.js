ab47d56c7da44c544ad546e2cf71cca5e84b685c
Elsa Bonde <elsasofiabonde@gmail.com>
2023-11-23 13:05:51 +0100

    Made html skeleton + func for calender, clock and showDate
    Co-authored-by: linusnilssson <linusnilssson@users.noreply.github.com>

--- public/main.js
+++ public/main.js
@@ -1,8 +1,12 @@
 window.addEventListener('DOMContentLoaded', main);
 
 function main() {
+  todaysDate();
+  updateClock();
+  setInterval(updateClock, 60000);
   console.log('Hi students!');
   // Init today view
   // Init todos list
   // Init calendar
 }
+
