b16e733e7ded260702e4db22a2bf2b56c3dcb1f6
ipieropan <igorpieropan@gmail.com>
2023-12-04 11:58:57 +0100

    fixed edit bug

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,130 +1,122 @@
-/**Open modal function */
-
+// Open modal function
 var existingEvents = JSON.parse(localStorage.getItem("events")) || [];
-let selectedId = ""; //håller koll på ID i event
+let selectedEventIndex = -1; // Holds the index of the selected event
 
 sortList(existingEvents);
 
 function openModal() {
-  document.getElementById("todoModal").style.display = "flex"; // Change 'flex' to 'block' if you prefer
+	document.getElementById("todoModal").style.display = "flex";
 }
 
 function closeModal() {
-  document.getElementById("todoModal").style.display = "none";
-  document.getElementById("editModal").style.display = "none";
+	document.getElementById("todoModal").style.display = "none";
+	document.getElementById("editModal").style.display = "none";
 }
 
 window.onclick = function (event) {
-  const modal = document.getElementById("todoModal");
-  const editModal = document.getElementById("editModal");
-  if (event.target == modal || event.target == editModal) {
-    modal.style.display = "none";
-  }
+	const modal = document.getElementById("todoModal");
+	const editModal = document.getElementById("editModal");
+	if (event.target == modal || event.target == editModal) {
+		modal.style.display = "none";
+	}
 };
 
 function addEvent() {
-  var title = document.getElementById("titleToDo").value;
-  var date = document.getElementById("dateToDo").value;
-  var time = document.getElementById("timeToDo").value;
+	var title = document.getElementById("titleToDo").value;
+	var date = document.getElementById("dateToDo").value;
+	var time = document.getElementById("timeToDo").value;
 
-  var newEvent = {
-    title: title,
-    date: date,
-    time: time,
-  };
+	var newEvent = {
+		title: title,
+		date: date,
+		time: time,
+		id: generateUniqueId(), // Function to generate a unique ID
+	};
 
-  var existingEvents = JSON.parse(localStorage.getItem("events")) || [];
-  existingEvents.push(newEvent);
-  localStorage.setItem("events", JSON.stringify(existingEvents));
+	var existingEvents = JSON.parse(localStorage.getItem("events")) || [];
+	existingEvents.push(newEvent);
+	localStorage.setItem("events", JSON.stringify(existingEvents));
 
-  updateEventList(existingEvents);
+	updateEventList(existingEvents);
 
-  closeModal();
+	closeModal();
 
-  loadEvents();
+	loadEvents();
+}
+
+function generateUniqueId() {
+	// Function to generate a unique ID
+	return Date.now().toString();
 }
 
 function updateEventList(events) {
-  const eventList = document.getElementById("eventList");
-  eventList.innerHTML = "";
+	const eventList = document.getElementById("eventList");
+	eventList.innerHTML = "";
 
-  events.forEach(function (event) {
-    const listItem = document.createElement("li");
+	events.forEach(function (event, index) {
+		const listItem = document.createElement("li");
 
-    const eventInfo = document.createElement("span");
-    eventInfo.className = "liEvents";
-    eventInfo.textContent = `${event.date} at ${event.time}\r\n${event.title}`;
-    listItem.appendChild(eventInfo);
+		const eventInfo = document.createElement("span");
+		eventInfo.className = "liEvents";
+		eventInfo.textContent = `${event.date} at ${event.time}\r\n${event.title}`;
+		listItem.appendChild(eventInfo);
 
-    const editButton = document.createElement("button");
-    editButton.className = "editButton";
-    editButton.onclick = function () {
-      handleEditClick(event, existingEvents);
-    };
-    listItem.appendChild(editButton);
+		const editButton = document.createElement("button");
+		editButton.className = "editButton";
+		editButton.addEventListener("click", function () {
+			handleEditClick(index);
+		});
+		listItem.appendChild(editButton);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "deleteButton";
-    deleteButton.dataset.cy = "delete-todo-button";
-    deleteButton.onclick = function () {
-      const listItem = this.parentNode;
-      const eventList = listItem.parentNode;
-      eventList.removeChild(listItem);
-    };
+		const deleteButton = document.createElement("button");
+		deleteButton.className = "deleteButton";
+		deleteButton.dataset.cy = "delete-todo-button";
+		listItem.appendChild(deleteButton);
 
-    listItem.appendChild(deleteButton);
-    eventList.appendChild(listItem);
-  });
+		eventList.appendChild(listItem);
+	});
 }
 
-function handleEditClick(event, existingEvents) {
-  const listItem = event.target.parentElement;
-  const index = Array.from(listItem.parentNode.children).indexOf(listItem);
-  const selectedEvent = existingEvents[index];
-  selectedId = selectedEvent.id;
+function handleEditClick(index) {
+	selectedEventIndex = index;
+	const selectedEvent = existingEvents[index];
 
-  fillEditModal(selectedEvent);
+	fillEditModal(selectedEvent);
 }
 
 function fillEditModal(selectedEvent) {
-  document.getElementById("editTitle").value = selectedEvent.title;
-  document.getElementById("editDate").value = selectedEvent.date;
-  document.getElementById("editTime").value = selectedEvent.time;
+	document.getElementById("editTitle").value = selectedEvent.title;
+	document.getElementById("editDate").value = selectedEvent.date;
+	document.getElementById("editTime").value = selectedEvent.time;
 
-  document.getElementById("editModal").style.display = "flex";
-}
-
-function updateListIdById(dataList, idToUpdate, newValues) {
-  const itemToUpdate = dataList.find((item) => item.id === idToUpdate);
-
-  if (idToUpdate) {
-    Object.assign(itemToUpdate, newValues);
-  }
+	document.getElementById("editModal").style.display = "flex";
 }
 
 function updateEventInLocalStorage() {
-  const existingEvents = JSON.parse(localStorage.getItem("events")) || [];
+	if (selectedEventIndex !== -1) {
+		const updatedTitle = document.getElementById("editTitle").value;
+		const updatedDate = document.getElementById("editDate").value;
+		const updatedTime = document.getElementById("editTime").value;
 
-  const updatedTitle = document.getElementById("editTitle").value;
-  const updatedDate = document.getElementById("editDate").value;
-  const updatedTime = document.getElementById("editTime").value;
+		const updatedEvent = {
+			title: updatedTitle,
+			date: updatedDate,
+			time: updatedTime,
+		};
 
-  const updatedEvent = {
-    title: updatedTitle,
-    date: updatedDate,
-    time: updatedTime,
-  };
+		existingEvents[selectedEventIndex] = updatedEvent;
 
-  updateListIdById(existingEvents, selectedId, updatedEvent);
+		localStorage.setItem("events", JSON.stringify(existingEvents));
 
-  localStorage.setItem("events", JSON.stringify(existingEvents));
+		updateEventList(existingEvents);
 
-  updateEventList(existingEvents);
+		closeModal();
+	}
 }
 
 function loadEvents() {
-  var existingEvents = JSON.parse(localStorage.getItem("events")) || [];
-  updateEventList(existingEvents);
+	var existingEvents = JSON.parse(localStorage.getItem("events")) || [];
+	updateEventList(existingEvents);
 }
 
 // Initial load on page load
