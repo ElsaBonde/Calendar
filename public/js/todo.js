4a2c6667ae626c2e135eaef427ea781dbf64f940
ipieropan <igorpieropan@gmail.com>
2023-12-11 11:01:24 +0100

    changes made by david

--- public/js/todo.js
+++ public/js/todo.js
@@ -5,184 +5,158 @@ sortList(existingEvents);
 
 // Open modal function
 function openModal() {
-  document.getElementById("saveToDoButton").onclick = addEvent;
-  document.getElementById("todoModal").style.display = "flex"; // Change 'flex' to 'block' if you prefer
+	document.getElementById("todoForm").onsubmit = addEvent;
+	document.getElementById("todoModal").style.display = "flex"; // Change 'flex' to 'block' if you prefer
 }
 
 // Function to close the modal
 function closeModal() {
-  document.getElementById("todoModal").style.display = "none";
+	document.getElementById("todoModal").style.display = "none";
 }
 
 // Close the modal if the user clicks outside the modal
 window.onclick = function (event) {
-  const modal = document.getElementById("todoModal");
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	const modal = document.getElementById("todoModal");
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 };
 
 // add new event function
-function addEvent() {
-  var title = document.getElementById("titleToDo").value;
-  var date = document.getElementById("dateToDo").value;
-  var time = document.getElementById("timeToDo").value;
+function addEvent(e) {
+	e.preventDefault();
+	var title = document.getElementById("titleToDo").value;
+	var date = document.getElementById("dateToDo").value;
+	var time = document.getElementById("timeToDo").value;
 
-  var newEvent = {
-    title: title,
-    date: date,
-    time: time,
-    id: title + date + time, //create an id for the event
-  };
+	var newEvent = {
+		title: title,
+		date: date,
+		time: time,
+		id: title + date + time, //create an id for the event
+	};
 
-  var existingEvents = JSON.parse(localStorage.getItem("todos")) || [];
-  existingEvents.push(newEvent);
+	var existingEvents = JSON.parse(localStorage.getItem("todos")) || [];
+	existingEvents.push(newEvent);
 
-  localStorage.setItem("todos", JSON.stringify(existingEvents));
+	localStorage.setItem("todos", JSON.stringify(existingEvents));
 
-  updateEventList(existingEvents);
-  closeModal();
-  loadEvents();
+	updateEventList(existingEvents);
+	closeModal();
+	loadEvents();
+	renderCalendar();
 }
 
 // sort list function (it sorts by date and time)
 function sortList(todos) {
-  const sortedList = todos.sort((a, b) => {
-    const aDateTime = `${a.date} ${a.time}`;
-    const bDateTime = `${b.date} ${b.time}`;
-    return aDateTime.localeCompare(bDateTime);
-  });
-  localStorage.setItem("todos", JSON.stringify(sortedList));
+	const sortedList = todos.sort((a, b) => {
+		const aDateTime = `${a.date} ${a.time}`;
+		const bDateTime = `${b.date} ${b.time}`;
+		return aDateTime.localeCompare(bDateTime);
+	});
+	localStorage.setItem("todos", JSON.stringify(sortedList));
 }
 
 // Function to update the event list on the page
 function updateEventList(todos) {
-  const eventList = document.getElementById("eventList");
-  eventList.innerHTML = "";
+	const eventList = document.getElementById("eventList");
+	eventList.innerHTML = "";
 
-  todos.forEach(function (event) {
-    var listItem = document.createElement("li");
+	todos.forEach(function (todo) {
+		var listItem = document.createElement("li");
 
-    // Create a span for event-info
-    var eventInfo = document.createElement("span");
-    eventInfo.className = "liEvents";
-    eventInfo.textContent = `${event.date} at ${event.time}\r\n${event.title}`;
+		// Create a span for event-info
+		var eventInfo = document.createElement("span");
+		eventInfo.className = "liEvents";
+		eventInfo.textContent = `${todo.date} at ${todo.time}\r\n${todo.title}`;
 
-    listItem.appendChild(eventInfo);
+		listItem.appendChild(eventInfo);
 
-    // edit button
-    const editButton = document.createElement("button");
-    editButton.setAttribute("data-cy", "edit-todo-button");
-    editButton.className = "editButton";
-    editButton.innerHTML =
-      '<i class="far fa-pen-to-square" style="color: #000000;"></i>';
-    editButton.addEventListener("click", (event) => {
-      handleEditClick(event, existingEvents);
-    });
-    listItem.appendChild(editButton);
-    eventList.appendChild(listItem);
+		// edit button
+		const editButton = document.createElement("button");
+		editButton.setAttribute("data-cy", "edit-todo-button");
+		editButton.className = "editButton";
+		editButton.innerHTML =
+			'<i class="far fa-pen-to-square" style="color: #000000;"></i>';
+		editButton.addEventListener("click", (event) => {
+			handleEditClick(todo);
+		});
+		listItem.appendChild(editButton);
+		eventList.appendChild(listItem);
 
-    // delete button
-    const deleteButton = document.createElement("button");
-    deleteButton.setAttribute("data-cy", "delete-todo-button");
-    deleteButton.className = "deleteButton";
-    deleteButton.innerHTML =
-      '<i class="fa-regular fa-trash-can" style="color: #000000;"></i>';
-    deleteButton.addEventListener("click", (event) => {
-      handleDeleteClick(event, existingEvents);
-    });
-    listItem.appendChild(deleteButton);
+		// delete button
+		const deleteButton = document.createElement("button");
+		deleteButton.setAttribute("data-cy", "delete-todo-button");
+		deleteButton.className = "deleteButton";
+		deleteButton.innerHTML =
+			'<i class="fa-regular fa-trash-can" style="color: #000000;"></i>';
+		deleteButton.addEventListener("click", (event) => {
+			handleDeleteClick(todo);
+		});
+		listItem.appendChild(deleteButton);
 
-    eventList.appendChild(listItem);
-  });
+		eventList.appendChild(listItem);
+	});
 }
 
 // edit function
-function handleEditClick(event, existingEvents) {
-  const listItem = event.target.closest("li");
-
-  const index = Array.from(listItem.parentNode.children).indexOf(listItem);
-
-  // Check if the index is valid
-  if (index === -1 || index >= existingEvents.length) {
-    console.error("Invalid index:", index);
-  }
-
-  const selectedEvent = existingEvents[index];
-  selectedId = selectedEvent.id;
-
-  fillEditModal(selectedEvent);
+function handleEditClick(todo) {
+  selectedId = todo.id;
+	fillEditModal(todo);
 }
 
 // delete function
-function handleDeleteClick(event, todos) {
-  const listItem = event.target.closest("li");
-
-  if (listItem && listItem.parentNode) {
-    if (listItem) {
-      const index = Array.from(listItem.parentNode.children).indexOf(listItem);
-
-      const removedEvent = todos.splice(index, 1)[0]; // Remove the event from the array
-      localStorage.setItem("todos", JSON.stringify(todos));
-      updateEventList(todos);
-
-      console.log("Deleted Event:", removedEvent);
-    }
-  }
+function handleDeleteClick(todoToRemove) {
+  // const index = todos.indexOf(todoToRemove)
+  // todos.splice(index, 1) // Remove the event from the array
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos = todos.filter(todo => todo.id !== todoToRemove.id)
+	localStorage.setItem("todos", JSON.stringify(todos));
+	updateEventList(todos);
+	renderCalendar();
 }
 
 function fillEditModal(selectedEvent) {
-  document.getElementById("titleToDo").value = selectedEvent.title;
-  document.getElementById("dateToDo").value = selectedEvent.date;
-  document.getElementById("timeToDo").value = selectedEvent.time;
-  document.getElementById("saveToDoButton").onclick = updateEventInLocalStorage;
+	document.getElementById("titleToDo").value = selectedEvent.title;
+	document.getElementById("dateToDo").value = selectedEvent.date;
+	document.getElementById("timeToDo").value = selectedEvent.time;
+	document.getElementById("todoForm").onsubmit = updateEventInLocalStorage;
 
-  document.getElementById("todoModal").style.display = "flex";
+	document.getElementById("todoModal").style.display = "flex";
 }
 
 function updateListIdById(dataList, idToUpdate, newValues) {
-  const itemToUpdate = dataList.find((item) => item.id === idToUpdate);
-  if (idToUpdate) {
-    Object.assign(itemToUpdate, newValues);
-  }
+	const itemToUpdate = dataList.find((item) => item.id === idToUpdate);
+	if (idToUpdate) {
+		Object.assign(itemToUpdate, newValues);
+	}
 }
 
-function updateEventInLocalStorage() {
-  const existingEvents = JSON.parse(localStorage.getItem("todos")) || [];
-  const updatedTitle = document.getElementById("titleToDo").value;
-  const updatedDate = document.getElementById("dateToDo").value;
-  const updatedTime = document.getElementById("timeToDo").value;
+function updateEventInLocalStorage(e) {
+	e.preventDefault();
+	const existingEvents = JSON.parse(localStorage.getItem("todos")) || [];
+	const updatedTitle = document.getElementById("titleToDo").value;
+	const updatedDate = document.getElementById("dateToDo").value;
+	const updatedTime = document.getElementById("timeToDo").value;
 
-  const updatedEvent = {
-    title: updatedTitle,
-    date: updatedDate,
-    time: updatedTime,
-  };
+	const updatedEvent = {
+		title: updatedTitle,
+		date: updatedDate,
+		time: updatedTime,
+	};
 
-  updateListIdById(existingEvents, selectedId, updatedEvent);
-  localStorage.setItem("todos", JSON.stringify(existingEvents)); // saves it to local storage
-  updateEventList(existingEvents);
+	updateListIdById(existingEvents, selectedId, updatedEvent);
+	localStorage.setItem("todos", JSON.stringify(existingEvents)); // saves it to local storage
+	updateEventList(existingEvents);
+	renderCalendar();
 }
 
 // Function to load todos from local storage and update the list on page load
 function loadEvents() {
-  var existingEvents = JSON.parse(localStorage.getItem("todos")) || [];
-  updateEventList(existingEvents);
+	var existingEvents = JSON.parse(localStorage.getItem("todos")) || [];
+	updateEventList(existingEvents);
 }
 
-document
-  .getElementById("eventList")
-  .addEventListener("click", function (event) {
-    const editButton = event.target.closest(".editButton");
-    const deleteButton = event.target.closest(".deleteButton");
-
-    if (editButton) {
-      handleEditClick(event, existingEvents);
-    } else if (deleteButton) {
-      handleDeleteClick(event, existingEvents);
-    }
-  });
-
 window.onload = function () {
-  loadEvents();
+	loadEvents();
 };
